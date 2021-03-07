887c38a67ad00177b7c1bd56056cc369c23c5a19
hinlocaesar <caesar.hinlo.tech@gmail.com>
2021-03-07 19:58:51 +0800

    Mind Map for Learning Web development

---
+++ Mind Maps/Review and Learning Plan.mm
@@ -0,0 +1,163 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1615110724971" ID="ID_1411112413" MODIFIED="1615115276632" TEXT="Review  Plan">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1615111296854" ID="ID_388119377" MODIFIED="1615115471789" POSITION="left" TEXT="REVIEW METHOD">
+<font BOLD="true" NAME="SansSerif" SIZE="14"/>
+<node CREATED="1615111313027" ID="ID_1101424785" MODIFIED="1615114858925" TEXT="">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1615114644240" ID="ID_1376969639" MODIFIED="1615114876131" TEXT="[Mind Map + Fyneman Technique + Linter Box]&#xa;1)Write everything you know on a sheet of paper.&#xa;2)Pretend to explain to a imaginary class &quot; to Find the gaps&quot;&#xa;3)Review those gaps&#xa;4) Create a simple example/metaphor.&#xa;5)Create notes for Lintnerbox&#xa;6)Repeat steps 2 to 4"/>
+</node>
+</node>
+<node CREATED="1615113487707" ID="ID_423298861" MODIFIED="1615115464758" POSITION="right" STYLE="fork" TEXT="FRONT END">
+<arrowlink DESTINATION="ID_423298861" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_1329088244" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_423298861" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_1329088244" SOURCE="ID_423298861" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<font BOLD="true" NAME="SansSerif" SIZE="15"/>
+<node COLOR="#000000" CREATED="1615110836867" ID="ID_889915586" MODIFIED="1615114592394" STYLE="fork" TEXT="Ajax &quot;YT Dev Ed&quot;">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="messagebox_warning"/>
+<node CREATED="1615110908462" ID="ID_511742379" MODIFIED="1615114592394" STYLE="fork" TEXT="Promises">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1615110914586" ID="ID_327456204" MODIFIED="1615114592394" STYLE="fork" TEXT="Async">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1615110920840" ID="ID_1214524745" MODIFIED="1615114592394" STYLE="fork" TEXT="Axios">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1615111285171" ID="ID_1927331184" MODIFIED="1615114592394" STYLE="fork" TEXT="Call Back">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1615113514593" ID="ID_476659508" MODIFIED="1615114592394" STYLE="fork" TEXT="HTML">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1615113636025" ID="ID_575281811" MODIFIED="1615114592394" STYLE="fork" TEXT="html?">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1615113900098" ID="ID_861744980" MODIFIED="1615114592394" STYLE="fork" TEXT="!HTML DOC TYPE">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1615113916171" ID="ID_2574226" MODIFIED="1615114592394" STYLE="fork" TEXT="&lt;title&gt;">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1615113930371" ID="ID_1223062395" MODIFIED="1615114592394" STYLE="fork" TEXT="&lt;head&gt;">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1615113942923" ID="ID_1043641503" MODIFIED="1615114592394" STYLE="fork" TEXT="&lt;body&gt;">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1615113951371" ID="ID_599795180" MODIFIED="1615114592394" STYLE="fork" TEXT="&lt;form action=&quot;&quot;&gt;">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1615113973715" ID="ID_1292844644" MODIFIED="1615114592394" STYLE="fork" TEXT="&lt;img&gt;">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1615113978396" ID="ID_555294867" MODIFIED="1615114592394" STYLE="fork" TEXT="&lt;a&gt;">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1615114077347" ID="ID_1209038455" MODIFIED="1615114592394" STYLE="fork" TEXT="list">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1615114084588" ID="ID_242618173" MODIFIED="1615114592394" STYLE="fork" TEXT="&lt;ul&gt;">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1615114092028" ID="ID_814861366" MODIFIED="1615114592394" STYLE="fork" TEXT="&lt;ol&gt;">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1615114111460" ID="ID_172241768" MODIFIED="1615114592394" STYLE="fork" TEXT="division">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1615114116396" ID="ID_849223495" MODIFIED="1615114592394" STYLE="fork" TEXT="section">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1615114169492" ID="ID_684747637" MODIFIED="1615114592394" STYLE="fork" TEXT="footer">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1615114185362" ID="ID_1327322107" MODIFIED="1615115744172" STYLE="fork" TEXT="??Others">
+<font NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="messagebox_warning"/>
+</node>
+</node>
+<node CREATED="1615114453453" ID="ID_906038483" MODIFIED="1615114592394" TEXT="button">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1615114461737" ID="ID_315603720" MODIFIED="1615114592394" TEXT="radio">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1615113524536" ID="ID_1120804408" MODIFIED="1615114592394" STYLE="fork" TEXT="CSS">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1615113535545" ID="ID_1557419183" MODIFIED="1615114592394" STYLE="fork" TEXT="Selectors">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1615113544040" ID="ID_867028644" MODIFIED="1615114592394" STYLE="fork" TEXT="FlexBox">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1615113549016" ID="ID_58790661" MODIFIED="1615114592394" STYLE="fork" TEXT="Grid">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1615113529456" ID="ID_336993083" MODIFIED="1615114592394" STYLE="fork" TEXT="JS">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1615114470660" ID="ID_1124112514" MODIFIED="1615114592394" TEXT="Framework">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1615114478627" ID="ID_169261677" MODIFIED="1615114592394" TEXT="Bulma">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1615114489297" ID="ID_664060983" MODIFIED="1615114592394" TEXT="BootStrap">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+</node>
+<node CREATED="1615113677289" ID="ID_871498587" MODIFIED="1615115433516" POSITION="right" STYLE="fork" TEXT="BACK END">
+<font BOLD="true" NAME="SansSerif" SIZE="15"/>
+<node CREATED="1615113683143" ID="ID_598416139" MODIFIED="1615114592394" STYLE="fork" TEXT="Node Js">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1615113688017" ID="ID_523752759" MODIFIED="1615114592394" STYLE="fork" TEXT="Express">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1615113695176" ID="ID_1006128086" MODIFIED="1615114592394" STYLE="fork" TEXT="Mongo">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1615115333658" ID="ID_39347235" MODIFIED="1615115344657" TEXT="NPM">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1615115350226" ID="ID_1038297185" MODIFIED="1615115359777" TEXT="Mongoose">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1615114931779" ID="ID_1716763823" MODIFIED="1615115479217" POSITION="left" TEXT="REVIEW SEQUENCE">
+<font BOLD="true" NAME="SansSerif" SIZE="14"/>
+<node CREATED="1615114983913" ID="ID_634798226" MODIFIED="1615115065174" TEXT="1)DevEd Video"/>
+<node CREATED="1615115040619" ID="ID_1955451769" MODIFIED="1615115078584" TEXT="2) Continue Mongoose Topic"/>
+<node CREATED="1615115082057" ID="ID_1988932799" MODIFIED="1615115217922" TEXT="3)Finish Colt Course"/>
+<node CREATED="1615115117459" ID="ID_470385572" MODIFIED="1615115221391" TEXT="4)Review Colts Course All Items"/>
+<node CREATED="1615115160346" ID="ID_441412801" MODIFIED="1615115248891">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      5)Study Andrei Neagoie Web Dev Boot camp<br />&quot;Supplement Knowledge Gaps&quot;<br />
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1615114964739" ID="ID_1605102890" MODIFIED="1615115504468" POSITION="right" TEXT="JUNIOR TO SENIOR">
+<font BOLD="true" NAME="SansSerif" SIZE="14"/>
+</node>
+<node CREATED="1615115497336" ID="ID_1830189438" MODIFIED="1615115520556" POSITION="right" TEXT="REACT ">
+<font BOLD="true" NAME="SansSerif" SIZE="14"/>
+</node>
+<node CREATED="1615115413521" ID="ID_176462698" MODIFIED="1615115513740" POSITION="right" TEXT="REACT NATIVE">
+<font BOLD="true" NAME="SansSerif" SIZE="14"/>
+</node>
+<node CREATED="1615115494281" ID="ID_1264143688" MODIFIED="1615115532954" POSITION="right" TEXT="20 JS PROJECTS">
+<font BOLD="true" NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</map>
