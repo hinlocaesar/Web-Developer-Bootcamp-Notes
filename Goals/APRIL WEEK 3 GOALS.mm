19d5ba0346df02dd0510520728b8394e49c6e639
Caesar <caesar.hinlo.tech@gmail.com>
2021-04-01 16:28:56 +0800

    Added Week 2 and Week3 for mindmap

---
+++ Goals/APRIL WEEK 3 GOALS.mm
@@ -0,0 +1,4 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1617265070285" ID="ID_1901806727" MODIFIED="1617265130640" TEXT="APRIL WEEK 3 GOALS"/>
+</map>
