4df17e7683cf107e4f9a0bc91b52cba6529a683b
Caesar <caesar.hinlo.tech@gmail.com>
2021-04-01 20:57:49 +0800

    Mindmap for react

---
+++ Mind Maps/React.mm
@@ -0,0 +1,52 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1616546272530" ID="ID_1130196278" MODIFIED="1616546286253" TEXT="React">
+<node CREATED="1616548698858" ID="ID_1100478944" MODIFIED="1616548721764" POSITION="right" TEXT="How to upgrade existing project to latest version">
+<node CREATED="1616548723802" ID="ID_116021540" MODIFIED="1616548774138" TEXT="1)Change package.json sion &quot;react-scripts&quot;"/>
+<node CREATED="1616548739857" ID="ID_886728147" MODIFIED="1616548746768" TEXT="2)run npm install"/>
+</node>
+<node CREATED="1616551537710" ID="ID_1198735815" MODIFIED="1616551560807" POSITION="right" TEXT="React is a view library &quot;little robot previous video&quot;">
+<node CREATED="1616551571949" ID="ID_1913547807" MODIFIED="1616551578493" TEXT="you can use vr, any device"/>
+</node>
+<node CREATED="1616552624912" ID="ID_428750205" MODIFIED="1616552688454" POSITION="left" TEXT="First react exercise">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1616551945347" ID="ID_1693619113" MODIFIED="1616552154897" TEXT="render() is always inculded">
+<node CREATED="1616552293795" ID="ID_246285806" MODIFIED="1616552307194" TEXT="return expect a single thing"/>
+</node>
+<node CREATED="1616552654602" ID="ID_1844606682" MODIFIED="1616552685166" TEXT="npm install tachyons, like bootstrap"/>
+<node CREATED="1616549822003" ID="ID_702387094" MODIFIED="1616549831665" TEXT="Temporaty for learning">
+<node CREATED="1616549832710" ID="ID_1374256683" MODIFIED="1616549848484" TEXT="use class instead of function use old code temp"/>
+</node>
+<node CREATED="1616552955804" ID="ID_954569882" MODIFIED="1616553043618" TEXT="for css  use className instead of class">
+<node CREATED="1616553349824" ID="ID_86308681" MODIFIED="1616553368618" TEXT="why becuase class is a reserved keyword in js"/>
+</node>
+<node CREATED="1616553060299" ID="ID_794355190" MODIFIED="1616553078423" TEXT="jsx is allowed us to write html to js"/>
+</node>
+<node CREATED="1616564040885" ID="ID_133290446" MODIFIED="1616564061394" POSITION="left" TEXT="One way data flow">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1616564063026" ID="ID_1365507928" MODIFIED="1616564068128" TEXT="props">
+<node CREATED="1616564068130" ID="ID_564355672" MODIFIED="1616564078774" TEXT="pass data from parent to child"/>
+<node CREATED="1616564079633" ID="ID_904435914" MODIFIED="1616564079633" TEXT=""/>
+</node>
+<node CREATED="1616564084225" ID="ID_1675021084" MODIFIED="1616564086507" TEXT="state">
+<node CREATED="1616564086508" ID="ID_1885967432" MODIFIED="1616564102842" TEXT="pass data from child to parent to child"/>
+<node CREATED="1616567413891" ID="ID_1257721545" MODIFIED="1616567431480" TEXT="is an object that describes your application"/>
+</node>
+<node CREATED="1616564166752" ID="ID_1712379243" MODIFIED="1616564228669">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="../../Pictures/one%20way%20data%20flow.PNG" />
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1616567462138" ID="ID_1054483871" MODIFIED="1616568808081" TEXT="rule props simply thing that come out of state">
+<icon BUILTIN="forward"/>
+</node>
+</node>
+<node CREATED="1616564162904" ID="ID_1210169513" MODIFIED="1616564162904" POSITION="left" TEXT=""/>
+</node>
+</map>
